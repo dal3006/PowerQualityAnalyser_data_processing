8f2ad87baa74711e3b00dafe15d1638854bbbdd0
timebis <48658392+timebis@users.noreply.github.com>
2022-05-22 13:52:55 +0000

    v20222205

--- main.py
+++ main.py
@@ -5,48 +5,45 @@ import utils
 import datetime
 import matplotlib.pyplot as plt
 
-#Inutile car la charge ne dispose pas de TC dans l'énoncé.
-# path_folder = r'data'
-# path_input_files = glob.glob(path_folder + "\\*.xls*")
-# columns_name_xlsx = ['Date:', 'Heure:'
-# ,'P1 (W)', 'P2 (W)', 'P3 (W)', 'PT (W)', 'Q1 (var)', 'Q2 (var)', 'Q3 (var)', 'QT (var)', 'S1 (VA)', 'S2 (VA)', 'S3 (VA)', 'ST (VA)'
-# ,'V1 rms MIN 1/2 période', 'V1 rms', 'V1 rms MAX 1/2 période', 'V2 rms MIN 1/2 période', 'V2 rms', 'V2 rms MAX 1/2 période', 'V3 rms MIN 1/2 période', 'V3 rms', 'V3 rms MAX 1/2 période', 'VNE MIN 1/2 période', 'VNE', 'VNE MAX 1/2 période'
-# ,'A1 rms MIN 1/2 période', 'A1 rms', 'A1 rms MAX 1/2 période', 'A2 rms MIN 1/2 période', 'A2 rms', 'A2 rms MAX 1/2 période', 'A3 rms MIN 1/2 période', 'A3 rms', 'A3 rms MAX 1/2 période', 'AN rms', 'AN rms MAX 1/2 période'
-# ,'Ep1 (Wh)', 'Ep2 (Wh)', 'Ep3 (Wh)', 'EpT (Wh)'
-# ,'Eq1 (varh)', 'Eq2 (varh)', 'Eq3 (varh)', 'EqT (varh)', 'Es1 (VAh)', 'Es2 (VAh)', 'Es3 (VAh)', 'EsT (VAh)'
-# ,'PF1', 'PF2', 'PF3', 'PFT'
-# ,'V1 THDr', 'V2 THDr', 'V3 THDr', 'VNE THDr', 'A1 THDf', 'A2 THDf', 'A3 THDf'
-# ,'Pst1', 'Pst2', 'Pst3', 'Plt1', 'Plt2', 'Plt3'
-# ]
-# columns_name = ['Date:','Heure:'
-# ,'p1(W)','p2(W)','p3(W)','pT(W)','q1(var)','q2(var)','q3(var)','qT(var)','s1(Va)','s2(Va)','s3(Va)','sT(Va)'
-# ,'V1_rms_min','V1_rms','V1_rms_max','V2_rms_min','V2_rms','V2_rms_max','V3_rms_min','V3_rms','V3_rms_max','VNE_min','VNE','VNE_max'
-# ,'a1_rms_min','a1_rms','a1_rms_max','a2_rms_min','a2_rms','a2_rms_max','a3_rms_min','a3_rms','a3_rms_max','aN_rms','aN_rms_max'
-# ,'Ep1_(Wh)','Ep2_(Wh)','Ep3_(Wh)','EpT_(Wh)'
-# ,'Eq1_(varh)','Eq2_(varh)','Eq3_(varh)','EqT_(varh)','Es1_(Vah)','Es2_(Vah)','Es3_(Vah)','EsT_(Vah)'
-# ,'pF1','pF2','pF3','pFT'
-# ,'V1_THDr','V2_THDr','V3_THDr','VNE_THDr','a1_THDf','a2_THDf','a3_THDf'
-# ,'pst1','pst2','pst3','plt1','plt2','plt3'
-# ]
-#
-# sheets_to_import = 'Enregistrement'
-#
-# for file in path_input_files:
-#     df_data = pd.read_excel(file, skiprows=[0, 2, 3, 4], header=0,usecols=columns_name_xlsx, sheet_name=sheets_to_import, parse_dates=False)  # Import data
-#     df_data.columns=columns_name
-#     df_data = utils.timestamp_creation(df_data)
-#
-#     for name_column in df_data.columns:
-#         print(name_column)
-#         #utils.plot_serie_temporelle(df_data,name_column)
-#
-#     df_data['i_moy'] = (df_data['a1_rms'] + df_data['a2_rms'] + df_data['a3_rms']) / 3
-#     df_data['K_des'] = (df_data['a1_rms'] * df_data['a1_rms'] + df_data['a2_rms'] * df_data['a2_rms'] + df_data[
-#         'a3_rms'] * df_data['a3_rms']) / (3 * df_data['i_moy'] * df_data['i_moy'])
+path_folder = r'data'
+path_input_files = glob.glob(path_folder + "\\*.xls*")
+columns_name_xlsx = ['Date:', 'Heure:'
+,'P1 (W)', 'P2 (W)', 'P3 (W)', 'PT (W)', 'Q1 (var)', 'Q2 (var)', 'Q3 (var)', 'QT (var)', 'S1 (VA)', 'S2 (VA)', 'S3 (VA)', 'ST (VA)'
+,'V1 rms MIN 1/2 période', 'V1 rms', 'V1 rms MAX 1/2 période', 'V2 rms MIN 1/2 période', 'V2 rms', 'V2 rms MAX 1/2 période', 'V3 rms MIN 1/2 période', 'V3 rms', 'V3 rms MAX 1/2 période', 'VNE MIN 1/2 période', 'VNE', 'VNE MAX 1/2 période'
+,'A1 rms MIN 1/2 période', 'A1 rms', 'A1 rms MAX 1/2 période', 'A2 rms MIN 1/2 période', 'A2 rms', 'A2 rms MAX 1/2 période', 'A3 rms MIN 1/2 période', 'A3 rms', 'A3 rms MAX 1/2 période', 'AN rms', 'AN rms MAX 1/2 période'
+,'Ep1 (Wh)', 'Ep2 (Wh)', 'Ep3 (Wh)', 'EpT (Wh)'
+,'Eq1 (varh)', 'Eq2 (varh)', 'Eq3 (varh)', 'EqT (varh)', 'Es1 (VAh)', 'Es2 (VAh)', 'Es3 (VAh)', 'EsT (VAh)'
+,'PF1', 'PF2', 'PF3', 'PFT'
+,'V1 THDr', 'V2 THDr', 'V3 THDr', 'VNE THDr', 'A1 THDf', 'A2 THDf', 'A3 THDf'
+,'Pst1', 'Pst2', 'Pst3', 'Plt1', 'Plt2', 'Plt3'
+]
+columns_name = ['Date:','Heure:'
+,'p1(W)','p2(W)','p3(W)','pT(W)','q1(var)','q2(var)','q3(var)','qT(var)','s1(Va)','s2(Va)','s3(Va)','sT(Va)'
+,'V1_rms_min','V1_rms','V1_rms_max','V2_rms_min','V2_rms','V2_rms_max','V3_rms_min','V3_rms','V3_rms_max','VNE_min','VNE','VNE_max'
+,'a1_rms_min','a1_rms','a1_rms_max','a2_rms_min','a2_rms','a2_rms_max','a3_rms_min','a3_rms','a3_rms_max','aN_rms','aN_rms_max'
+,'Ep1_(Wh)','Ep2_(Wh)','Ep3_(Wh)','EpT_(Wh)'
+,'Eq1_(varh)','Eq2_(varh)','Eq3_(varh)','EqT_(varh)','Es1_(Vah)','Es2_(Vah)','Es3_(Vah)','EsT_(Vah)'
+,'pF1','pF2','pF3','pFT'
+,'V1_THDr','V2_THDr','V3_THDr','VNE_THDr','a1_THDf','a2_THDf','a3_THDf'
+,'pst1','pst2','pst3','plt1','plt2','plt3'
+]
 
-path_file_solar = path_folder + "\Plant_2_Generation_Data.csv"
-df_solar_data = pd.read_csv(path_file_solar,parse_dates=['DATE_TIME']).set_index('DATE_TIME')
-df_solar_data = df_solar_data[df_solar_data['SOURCE_KEY']==df_solar_data['SOURCE_KEY'][0]] #Selection of one string
-df_solar_data=df_solar_data.asfreq('5T',method='ffill')
+sheets_to_import = 'Enregistrement'
 
-df_solar_data['ppoc_measured']=np.random.randint(50,800), size=len(df_solar_data))
+for file in path_input_files:
+    df_data = pd.read_excel(file, skiprows=[0, 2, 3, 4], header=0,usecols=columns_name_xlsx, sheet_name=sheets_to_import, parse_dates=False)  # Import data
+    df_data = utils.timestamp_creation(df_data)
+    # df_data.rename(str.lower.replace(' ','_'), axis='columns',inplace=True)
+    df_data.columns = (x.lower().replace(' ', '_').replace('(', '').replace(')', '') for x in df_data.columns)
+
+    # df_data.columns=columns_name
+
+    for name_column in df_data.columns:
+        if name_column=='sT(Va)':   #Selection
+            print(name_column)
+            utils.plot_serie_temporelle(df_data,name_column)
+            # utils.plot_serie_temporelle(df_data,'PT (W)')
+
+    df_data['i_moy'] = (df_data['a1_rms'] + df_data['a2_rms'] + df_data['a3_rms']) / 3
+    df_data['K_des'] = (df_data['a1_rms'] * df_data['a1_rms'] + df_data['a2_rms'] * df_data['a2_rms'] + df_data[
+        'a3_rms'] * df_data['a3_rms']) / (3 * df_data['i_moy'] * df_data['i_moy'])
